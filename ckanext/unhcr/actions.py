0592b6235924282a909d29f6f718aa90470f30bd
chris48s <chris.shaw480@gmail.com>
2021-05-18 12:52:07 +0100

    don't allow container admins to change their own role

--- ckanext/unhcr/actions.py
+++ ckanext/unhcr/actions.py
@@ -1256,6 +1256,7 @@ def user_autocomplete(context, data_dict):
 
     q = data_dict['q']
     limit = data_dict.get('limit', 20)
+    ignore_self = data_dict.get('ignore_self', False)
 
     query = model.User.search(q)
     query = query.filter(model.User.state != model.State.DELETED)
@@ -1265,6 +1266,8 @@ def user_autocomplete(context, data_dict):
             for domain in utils.get_internal_domains()
         ]
         query = query.filter(or_(*conditions))
+    if ignore_self:
+        query = query.filter(model.User.name != user)
     query = query.limit(limit)
 
     user_list = []
