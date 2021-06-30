d4b9d9aad31bcd757c0b9e297a936d96e84be3c0
chris48s <chris.shaw480@gmail.com>
2021-06-30 09:49:53 +0100

    Revert "run the tests on python3"
    This reverts commit a6f14beeeb8336557623e7ed6f4cac19a66cb03e.

--- bin/install-unhcr-2.9-py2.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-python setup.py develop
-pip install --upgrade -r requirements.txt
-pip install --upgrade -r dev-requirements.txt
--- bin/install-unhcr-2.9.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-python3 setup.py develop
-pip install --upgrade -r requirements.txt
-pip install --upgrade -r dev-requirements.txt
