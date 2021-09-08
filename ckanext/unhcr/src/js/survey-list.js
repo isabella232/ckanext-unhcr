989240b2a71871f14a9d34d6f7a67f7cf020c7b1
Andres Vazquez <andres@data99.com.ar>
2021-09-08 13:12:04 -0300

    Allow sorting and searching at KoBo survey list (#683)
    Allow sorting and searching at KoBo survey list
    Co-authored-by: amercader <amercadero@gmail.com>

--- ckanext/unhcr/src/js/survey-list.js
+++ ckanext/unhcr/src/js/survey-list.js
@@ -4,8 +4,7 @@ $( document ).ready(function() {
   kobo-pkg-update-resources button starts a job to update all resources
   with new submissions
   */
-  $('.kobo-pkg-update-resources').click(
-    function(evt) {
+  $(document).on("click", ".kobo-pkg-update-resources", function(){
       $this = $(this);
       let kobo_asset_id = $this.data('kobo-asset-id');
       let url = $this.data('kobo-update-endpoint');
@@ -19,13 +18,21 @@ $( document ).ready(function() {
             $("#update-kobo-resources-container-" + kobo_asset_id).addClass('alert alert-success running-kobo-update');
 					},
 					error: function (xhr) {
-						alert('Failed to start a job to update this survey ' + xhr.responseJSON.error)
+						alert('Failed to start a job to update this survey ' + xhr.responseJSON.error);
 					}
         }
       );
 
     }
-  )
+  );
 
+	$("#survey-list-table").fancyTable({
+		sortColumn:2,
+		pagination: true,
+		inputPlaceholder: "Filter table...",
+		perPage:10,
+		globalSearch:true,
+		inputStyle: "width: inherit"
+	});
 
 });
