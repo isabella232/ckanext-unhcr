03ed0b56e690c2dfcf77e8b4980721f9bf9eee61
Andres Vazquez <andres@data99.com.ar>
2021-09-10 11:27:42 -0300

    Fix tests

--- ckanext/unhcr/mailer.py
+++ ckanext/unhcr/mailer.py
@@ -338,7 +338,7 @@ def compose_request_access_email_body(object_type, recipient, obj, requesting_us
             model.Group.get(container_id)
             for container_id in obj['default_containers']
         ]
-        container_titles = [container.title for container in containers if container.name != 'data-deposit']
+        container_titles = [container.title for container in containers if container and container.name != 'data-deposit']
         obj['container_titles'] = container_titles
 
     return render_jinja2('emails/access_requests/access_request.html', context)
